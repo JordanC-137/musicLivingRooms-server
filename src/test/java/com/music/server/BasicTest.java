a054ae1d1044eb5bb65a7a1e917cf29608fa6d49
Jordan <jordanasiama3@gmail.com>
2026-07-29 00:10:51 +0100

    First test

---
+++ src/test/java/com/music/server/BasicTest.java
@@ -0,0 +1,22 @@
+package com.music.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.client.RestTestClient;
+
+@AutoConfigureRestTestClient
+@SpringBootTest
+class BasicTest {
+
+    @Autowired
+    private RestTestClient restTestClient;
+
+    @Test
+    void confirmGet(){
+        restTestClient.get().uri("/albums")
+        .exchange()
+        .expectStatus().isOk();
+    }
+}
