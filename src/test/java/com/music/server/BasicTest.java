671e0c6c881b310d81ce0051044577a526f6164b
Jordan <jordanasiama3@gmail.com>
2026-07-29 06:47:43 +0100

    Test name change and removal of pre-loading database

--- src/test/java/com/music/server/BasicTest.java
+++ src/test/java/com/music/server/BasicTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.client.RestTestClient;
+import org.springframework.http.MediaType;
 
 @AutoConfigureRestTestClient
 @SpringBootTest
@@ -14,9 +15,9 @@ class BasicTest {
     private RestTestClient restTestClient;
 
     @Test
-    void confirmGet(){
+    void confirmEmptyInitialGet(){
         restTestClient.get().uri("/albums")
         .exchange()
-        .expectStatus().isOk();
+        .expectBody().json("[]");
     }
 }
